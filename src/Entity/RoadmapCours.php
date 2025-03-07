ddce28683d861cd9a30c306c8f57a22443529030
wissalbelhadjamorr <wissalbelhadjamor12@gmail.com>
2025-03-07 14:02:19 +0100

    roadmap relations

--- src/Entity/RoadmapCours.php
+++ src/Entity/RoadmapCours.php
@@ -16,6 +16,14 @@ class RoadmapCours
     #[ORM\Column]
     private ?int $ord = null;
 
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?roadmaps $roadmap_id = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?cours $cours_id = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -29,6 +37,30 @@ class RoadmapCours
     public function setOrd(int $ord): static
     {
         $this->ord = $ord;
+
+        return $this;
+    }
+
+    public function getRoadmapId(): ?roadmaps
+    {
+        return $this->roadmap_id;
+    }
+
+    public function setRoadmapId(?roadmaps $roadmap_id): static
+    {
+        $this->roadmap_id = $roadmap_id;
+
+        return $this;
+    }
+
+    public function getCoursId(): ?cours
+    {
+        return $this->cours_id;
+    }
+
+    public function setCoursId(?cours $cours_id): static
+    {
+        $this->cours_id = $cours_id;
 
         return $this;
     }
