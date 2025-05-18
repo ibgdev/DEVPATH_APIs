0b385f916c6514497dcc1ca267795aa600c9339d
ibgdev <ibrahimghorbali605@gmail.com>
2025-05-18 15:35:20 +0100

    constraints fixes

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -30,28 +30,36 @@ final class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
+#[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
+public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+{
+    $form = $this->createForm(UserType::class, $user);
+    $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
+    if ($form->isSubmitted() && $form->isValid()) {
+        $existingUser = $entityManager->getRepository(User::class)->findOneBy(['email' => $user->getEmail()]);
 
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        if ($existingUser && $existingUser->getId() !== $user->getId()) {
+            $this->addFlash("error", "Email is already used by another user.");
+            return $this->redirectToRoute('app_user_index');
         }
 
-        return $this->render('user/edit.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
+        $this->addFlash("success", "User updated sucssefuly.");
+        $entityManager->flush();
+        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
     }
+
+    return $this->render('user/edit.html.twig', [
+        'user' => $user,
+        'form' => $form,
+    ]);
+}
+
 
     #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
     public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->getPayload()->getString('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->getPayload()->getString('_token'))) {
             if (count($user->getProgressions()) > 0) {
                 foreach ($user->getProgressions() as $prog) {
                     $entityManager->remove($prog);
